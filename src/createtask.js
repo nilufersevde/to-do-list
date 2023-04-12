3e006ec216a1d888c1faf4b92587ad20a97b0e45
Nilufer <n.sevde@hotmail.com>
2023-04-12 18:09:39 +0300

    add html skeleton and factory function of task

---
+++ src/createtask.js
@@ -0,0 +1,17 @@
+const CreateTask = (title, description, dueDate, importance = low, completed = incomplete) =>{
+    function setCompleted(completed) {
+        this.completed = completed;
+    }
+    function setImportance(importance) {
+        this.importance = importance;
+    }
+    return{
+        title,
+        description,
+        completed,
+        dueDate,
+        importance,
+        setCompleted,
+        setImportance,
+    }
+}
